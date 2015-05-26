eec9444dc4ca4cf9539f259aa8822dfbe002112c
David Turton <dturton@etrivo.com>
2015-05-26 13:00:10 -0400

    added years module

---
+++ common/models/years.js
@@ -0,0 +1,7 @@
+module.exports = function(Years) {
+	var isStatic = true;
+	// Years.disableRemoteMethod('deleteById', isStatic);
+	// Years.disableRemoteMethod('upsert', isStatic);
+	// Years.disableRemoteMethod('create', isStatic);
+	// Years.disableRemoteMethod('prototype.updateAttributes', isStatic);
+};
