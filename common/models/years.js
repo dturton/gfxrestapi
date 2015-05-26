20be1745cbca96bfd1ac47d9cd71a113866efc68
David Turton <dturton@etrivo.com>
2015-05-26 14:06:27 -0400

    added unique method

--- common/models/years.js
+++ common/models/years.js
@@ -1,7 +1,35 @@
 module.exports = function(Years) {
 	var isStatic = true;
-	// Years.disableRemoteMethod('deleteById', isStatic);
-	// Years.disableRemoteMethod('upsert', isStatic);
-	// Years.disableRemoteMethod('create', isStatic);
-	// Years.disableRemoteMethod('prototype.updateAttributes', isStatic);
+	Years.disableRemoteMethod('deleteById', isStatic);
+	Years.disableRemoteMethod('upsert', isStatic);
+	Years.disableRemoteMethod('create', isStatic);
+	Years.disableRemoteMethod('prototype.updateAttributes', isStatic);
+
+    Years.getYears = function(cb) {
+      Years.find( function(err, years) { 
+      	cb(null, years.getUnique('yearid'));
+       });
+    }
+
+
+	Years.remoteMethod(
+	    'getYears', 
+	    {
+	       returns: {arg: 'years', type: 'string'},
+	       http:    {path: '/get', verb: 'get'}
+	     }
+	);
+
+	Array.prototype.getUnique = function(t){
+		var flags = [], output = [], l = this.length, i;
+		for( i=0; i<l; i++) {
+		    if( flags[this[i][t]]) continue;
+		    flags[this[i][t]] = true;
+		    output.push(this[i][t]);
+		}
+		return output;
+	}
+
 };
+
+
