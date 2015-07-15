cc679f3886819ca3ea8b4075cf65255ecb9b72ea
David Turton <dturton@etrivo.com>
2015-07-15 16:25:03 -0400

    Revert "new makes model"
    This reverts commit 4d2b0299b6a6f0ed8abfb9a03da84b2ebc71b51a.

--- common/models/makes.js
+++ common/models/makes.js
@@ -5,31 +5,11 @@ module.exports = function(Makes) {
 	Makes.disableRemoteMethod('create', isStatic);
 	Makes.disableRemoteMethod('prototype.updateAttributes', isStatic);
 	Makes.disableRemoteMethod('find', isStatic);
-
 	
 	Makes.getMakes = function(year, cb) {
       Makes.find({where: {yearid: year} }, function(err, makes) { 
       	cb(null, makes)
        });
-    };
-
-
-	Makes.getAllMakes = function(year, cb) {
-		var ds = Makes.dataSource;
-		var sql = "select DISTINCT MakeName from results2 where yearid =?";
-		var year = [year];
-		var makesarray = [];
-
-		ds.connector.query(sql, year, function (err, makes) {
-            if (err) console.error(err);
-            
-			for (i = 0; i < makes.length; i++) { 
-			    makesarray.push(makes[i].MakeName);
-			}
-            cb(err, makesarray);
-
-
-        });
     }
 
 
@@ -42,12 +22,4 @@ module.exports = function(Makes) {
 	     }
 	);
 
-	Makes.remoteMethod(
-		'getAllMakes', 
-	    {
-	       accepts: {arg: 'year', type: 'string', requiered: true},
-	       returns: {arg: 'makes', type: 'string'},
-	       http:    {path: '/getall', verb: 'get'}
-	     }
-	);
 };
